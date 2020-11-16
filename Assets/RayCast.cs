f7001596922070f2ed29a4c2dd09e4188d4576e7
4nd3l1 <daniel.goncalves9985@gmail.com>
2020-11-16 01:29:47 +0000

    Snow Globe attached to the left hand

--- Assets/RayCast.cs
+++ Assets/RayCast.cs
@@ -9,19 +9,32 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class RayCast : MonoBehaviour
 {
     public LineRenderer line;
-    public GameObject sphere;
+    public GameObject sphere; 
+    public GameObject snowglobe_prefab;
+    public GameObject leftController_GO;
     private InputDevice rightController;
     private InputDevice leftController;
     private GameObject sphere_rendered;
     private float distance = 0;
     private float x_trans = 0;
-    private List<GameObject> snowglobe_objs = new List<GameObject>();
+    private List <GameObject> snowglobe_objs = new List<GameObject>(); 
+    private List<GameObject> snowglobe_orig = new List<GameObject>();
+
+
+    private GameObject snowGlobe;
+
+    private bool mode = false;
+    private bool last_A = false;
+
+    public class SnowGlobe_Tuple
+    {
+        public GameObject copy, original;
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
         List<InputDevice> devices_right = new List<InputDevice>();
         List<InputDevice> devices_left = new List<InputDevice>();
         InputDeviceCharacteristics ControllerCharacteristics = InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right;
@@ -31,94 +44,109 @@ public class RayCast : MonoBehaviour
         InputDevices.GetDevicesWithCharacteristics(ControllerCharacteristics, devices_left);
         leftController = devices_left[0];
         sphere_rendered = Instantiate(sphere, transform.position, Quaternion.identity);
+
+        snowGlobe = Instantiate(snowglobe_prefab);
+        snowGlobe.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Raycast drawing //////////////////////////////////////
-        rightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 joystick_right);
-        distance += joystick_right.y * 0.1f;
-        if (distance < 0)
+        // snow globe sphere //////////////////////////////////////
+
+        Vector3 last_pos = snowGlobe.transform.position;
+        snowGlobe.transform.position = new Vector3(leftController_GO.transform.position.x,
+            leftController_GO.transform.position.y + 0.2f, leftController_GO.transform.position.z);
+        Vector3 mov = last_pos - snowGlobe.transform.position;
+        
+        foreach (var item in snowglobe_objs)
         {
-            distance = 0;
+            item.transform.position = item.transform.position - mov;
         }
-        else if(distance > 1000)
+
+        
+        rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out bool grip_right);
+
+        if (grip_right)         
         {
-            distance = 1000;
-        }
-        sphere_rendered.transform.position = transform.position + transform.TransformDirection(Vector3.forward) * distance;
-        leftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 joystick_left);
-        if(joystick_left.y > 0)
-        {
-            sphere_rendered.transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
-        } 
-        else if(joystick_left.y < 0)
-        {
-            if(sphere_rendered.transform.localScale.x > 0)
+            sphere_rendered.SetActive(true);
+            line.enabled = true;
+
+            // Raycast drawing //////////////////////////////////////
+            rightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 joystick_right);
+            distance += joystick_right.y * 0.1f;
+            if (distance < 0)
             {
-                sphere_rendered.transform.localScale -= new Vector3(0.05f, 0.05f, 0.05f);
+                distance = 0;
             }
-
-        }
-
-
-        line.SetPosition(0, transform.position);
-        line.SetPosition(1, transform.TransformDirection(Vector3.forward) * 1000);
-        line.enabled = true;
-
-        // Raycast selection //////////////////////////////////////
-
-        leftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool button_X);
-        leftController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool button_Y);
-        collision col_script = (GameObject.FindGameObjectsWithTag("Sphere")[0]).GetComponent<collision>();
-        List<GameObject> selection = col_script.selection;
-        if (button_X)
-        {
-            foreach (var item in snowglobe_objs)
+            else if (distance > 1000)
             {
-                GameObject.Destroy(item);
-
+                distance = 1000;
             }
-            selection = col_script.selection;
-            foreach (var item in selection)
+            sphere_rendered.transform.position = transform.position + transform.TransformDirection(Vector3.forward) * distance;
+            leftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 joystick_left);
+            if (joystick_left.y > 0)
             {
-                if (!snowglobe_objs.Contains(item))
+                sphere_rendered.transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+            }
+            else if (joystick_left.y < 0)
+            {
+                if (sphere_rendered.transform.localScale.x > 0)
                 {
-                    GameObject copy = Instantiate(item);
-                    Destroy(copy.GetComponent<Rigidbody>());
-                    copy.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-                    copy.transform.position = new Vector3(Camera.main.transform.position.x + copy.transform.position.x*0.1f, transform.position.y,
-                        Camera.main.transform.position.z + copy.transform.position.z * 0.1f + 0.2f);
-                    snowglobe_objs.Add(copy);
+                    sphere_rendered.transform.localScale -= new Vector3(0.05f, 0.05f, 0.05f);
                 }
-                
-            }
-        } else if (button_Y)
-        {
-            foreach (var item in snowglobe_objs)
-            {
-                GameObject.Destroy(item);
-            }
-            col_script.selection.Clear();
-        }
 
-        /*
-        int layerMask = 1 << 8;
-        layerMask = ~layerMask; 
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            //Debug.Log("Did Hit");
+            }
+
+
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, transform.TransformDirection(Vector3.forward) * 1000);
+            line.enabled = true;
+
+            // Raycast selection //////////////////////////////////////
+
+            leftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool button_X);
+            leftController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool button_Y);
+
+
+            collision col_script = (GameObject.FindGameObjectsWithTag("Sphere")[0]).GetComponent<collision>();
+            List<GameObject> selection = col_script.selection;
+
+            if (button_X)
+            {
+                foreach (var item in snowglobe_objs)
+                {
+                    GameObject.Destroy(item);
+                }
+                snowglobe_objs.Clear();
+                selection = col_script.selection;
+                foreach (var item in selection)
+                {
+                    if (!snowglobe_objs.Contains(item))
+                    {
+                        float radius = sphere_rendered.transform.localScale.x * sphere_rendered.GetComponent<SphereCollider>().radius;
+                        float scaler = 0.3f / (sphere_rendered.transform.localScale.x);
+                        Debug.Log(radius);
+                        GameObject copy = Instantiate(item);
+                        Destroy(copy.GetComponent<Rigidbody>());
+                        copy.transform.localScale = new Vector3(scaler, scaler, scaler);
+                        float x = snowGlobe.transform.position.x + scaler * (copy.transform.position.x - sphere_rendered.transform.position.x);
+                        float y = snowGlobe.transform.position.y + scaler * (copy.transform.position.y - sphere_rendered.transform.position.y);
+                        float z = snowGlobe.transform.position.z + scaler * (copy.transform.position.z - sphere_rendered.transform.position.z);
+                        copy.transform.position = new Vector3(x, y, z);
+                        snowglobe_objs.Add(copy);
+                        snowglobe_orig.Add(item);
+                    }
+
+                }
+            }
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            //Debug.Log("Did not Hit");
+            sphere_rendered.SetActive(false);
+            line.enabled = false;
         }
-        */
+
 
     }
 
